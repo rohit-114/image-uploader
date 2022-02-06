50aee5ffca2b20ef3925d7dd19ff695174cdbdfe
Rohit <shinderohit004@gmail.com>
2022-02-06 15:23:26 +0530

    change index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
